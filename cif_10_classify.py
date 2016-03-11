1588580ba9f034ab1d5b9ef61076e92afc3094b4
HarshUpadhyay <Harsh.Upadhyay@rice.edu>
2016-03-11 00:55:18 -0600

    initial commit

---
+++ cif_10_classify.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+
+from time import time
+import logging
+import matplotlib.pyplot as plt
+import image_utils
+from skimage.data import imread
+
+from sklearn.cross_validation import train_test_split
+from sklearn.datasets import fetch_lfw_people
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.decomposition import RandomizedPCA
+from sklearn.svm import SVC
+
+#X_train, y_train, X_val, y_val, X_test, y_test = image_utils.get_CIFAR10_data()
+y = imread("")
+print("done")
