ba875716bb4d26b29cef95db5a26c100e43825d4
HarshUpadhyay <Harsh.Upadhyay@rice.edu>
2016-03-29 16:58:59 -0500

    refactoring

--- test.py
+++ test.py
@@ -1,12 +1,24 @@
 import utils
+from sknn.mlp import Classifier, Convolution, Layer
+import numpy as np
 import image_utils
-import matplotlib.pyplot as plt
-import cPickle
 
 
-utils.save_test_data_as_vector("test.dat",  "test")
-#X=utils.read_test_data('testData/test0.dat')
-#print len(X)
-#plt.imshow(X[0].astype('uint8'))
-#plt.savefig('tmp_img.pdf')
-#plt.close()
+utils.save_training_data_as_vector("test.dat", "trainLabels.csv", "test")
+'''
+X, y = utils.read_training_data('')
+
+nn = Classifier(
+    layers=[
+        Convolution("Rectifier", channels=16, kernel_shape=(5,5)),
+        Layer("Softmax")],
+        regularize="L2",
+    n_iter=1000)
+nn.fit(X, y)
+
+pred = nn.predict(X)
+
+accuracy = np.mean((y == pred).astype(float)) * 100
+
+print "accuracy: {}".format(accuracy)
+'''
