cc1f76a208ad4e213bf111b95255e86589bb0104
HarshUpadhyay <Harsh.Upadhyay@rice.edu>
2016-04-11 21:31:37 -0500

    added code to "Save" our model

--- kerasCnn.py
+++ kerasCnn.py
@@ -77,12 +77,12 @@ else:
 
     # this will do preprocessing and realtime data augmentation
     datagen = ImageDataGenerator(
-        featurewise_center=True,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=True,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=True,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=True,  # divide each input by its std
         zca_whitening=True,  # apply ZCA whitening
-        rotation_range=0.1,  # randomly rotate images in the range (degrees, 0 to 180)
+        rotation_range=180,  # randomly rotate images in the range (degrees, 0 to 180)
         width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
         height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
         horizontal_flip=True,  # randomly flip images
@@ -98,3 +98,7 @@ else:
                         nb_epoch=nb_epoch, show_accuracy=True,
                         validation_data=(X_val, Y_val),
                         nb_worker=1)
+
+    import cPickle
+    #save model
+    cPickle.dump(model, "keras1.model");
